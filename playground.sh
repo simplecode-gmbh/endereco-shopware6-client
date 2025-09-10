a9e69673774660c8baf3bb91652f82ed7e221d68
Ilja Weber <ilja@endereco.de>
2025-09-10 10:40:05 +0200

    Update shopware compatibility to 6.7.2.0
    Update phpstan configuration and various scripts to support 6.7.2.0 version of shopware.
    Unfortunately the 6.7.2.1 version of dockware is not yet released, so we use a slightly older
    version of shop, which is stil lthe new feature release and therefore good for testing.

--- playground.sh
+++ playground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of supported Shopware versions
-declare -a versions=("6.7.0.1" "6.7.1.2")
+declare -a versions=("6.7.0.1" "6.7.1.2" "6.7.2.0")
 
 # Function to check if an element is in the array
 containsElement () {
